b968d12c3eead40247cb43ce8027cc01f6d372d0
pc <pc@>
2026-03-05 13:00:07 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace WinFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            button1 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("맑은 고딕", 15F);
+            button1.Location = new Point(777, 547);
+            button1.Name = "button1";
+            button1.Size = new Size(382, 77);
+            button1.TabIndex = 0;
+            button1.Text = "배경색 바꾸기";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("맑은 고딕", 20F);
+            label1.Location = new Point(340, 112);
+            label1.Name = "label1";
+            label1.Size = new Size(192, 72);
+            label1.TabIndex = 1;
+            label1.Text = "박진철";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("맑은 고딕", 15F);
+            label2.Location = new Point(340, 220);
+            label2.Name = "label2";
+            label2.Size = new Size(473, 54);
+            label2.TabIndex = 2;
+            label2.Text = "수원대 컴퓨터학부 2학년";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("맑은 고딕", 15F);
+            label3.Location = new Point(340, 307);
+            label3.Name = "label3";
+            label3.Size = new Size(744, 54);
+            label3.TabIndex = 3;
+            label3.Text = "010-3863-7830 pjc5767830@gmail.com";
+            label3.Click += label3_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("맑은 고딕", 15F);
+            label4.Location = new Point(340, 385);
+            label4.Name = "label4";
+            label4.Size = new Size(411, 54);
+            label4.TabIndex = 4;
+            label4.Text = "천안시 서북구 쌍용동";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(46, 112);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(243, 335);
+            pictureBox1.TabIndex = 5;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(14F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1270, 696);
+            Controls.Add(pictureBox1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private PictureBox pictureBox1;
+    }
+}
